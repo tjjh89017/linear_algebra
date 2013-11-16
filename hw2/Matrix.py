5af729921ea70fa33b6eb8b3a94e17a7cde953e1
Date <tjjh89017@hotmail.com>
2013-11-16 23:56:10 +0800

    fix

---
+++ hw2/Matrix.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+class Matrix:
+
+	def __init__(self, matrix = [[]]):
+		
+		self.matrix = matrix
+		self.height = len(matrix)
+		self.width = len(matrix[0])
+		# I think I should check this matrix is valid
+
+	def __add__(self, b):
+
+		m = []
+		for i in xrange(0, self.height):
+			m.append([val[0] + val[1] for val in zip(self.matrix[i], b.matrix[i])])
+		return Matrix(m)
+
+	def __sub__(self, b):
+
+		m = []
+		for i in xrange(0, self.height):
+			m.append([val[0] - val[1] for val in zip(self.matrix[i], b.matrix[i])])
+		return Matrix(m)
+
+	def __mul__(self, b):
+
+		rtn = []
+		m = self.matrix
+		n = zip(*(b.matrix))
+		for i in xrange(0, self.height):
+			rtn.append([])
+			for j in xrange(0, self.width):
+				rtn[i].append(sum([val[0] * val[1] for val in zip(m[i], n[j])]))
+		
+		return Matrix(rtn)
+
+
+if __name__ == '__main__':
+
+	a = Matrix([[1, 2], [3, 4]])
+	b = Matrix([[1, 0], [0, 1]])
+
+	print (a + b).matrix
+	print (a - b).matrix
+	print (a * b).matrix
